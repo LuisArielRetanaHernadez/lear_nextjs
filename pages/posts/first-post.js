a8e1a7bd61cea365a2f88a426f1b4e41b822d1b7
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-09 17:48:14 -0600

    init nextjs

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Firts Page</h1>
+      <h2>
+        <Link href="/">Bakc to home</Link>
+      </h2>
+    </>
+  )
+}
